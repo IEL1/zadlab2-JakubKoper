58b6c069458cbbc63123dc7715b83b8aa837b0be
Kuba <Kuba@192.168.1.101>
2018-03-11 17:35:29 +0100

    Zadanie 5

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -11,6 +11,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO zad 3, 4, 6, 7
+        System.out.println("To jest zadanie numer 5:");
+        for (int i = 20; i > 0; i--) {
+            if (i == 2 || i == 6 || i == 9 || i == 15 || i == 19) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
-    
+
 }
